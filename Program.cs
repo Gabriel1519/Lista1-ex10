b8aa4d75eec5c8cb0d0dbd652a3c56347e738e3f
Gabriel1519 <130614709+Gabriel1519@users.noreply.github.com>
2023-04-17 19:40:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1Ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Real;
+            double Dolar;
+            double Cotação;
+
+            Console.Write("Insira a Cotação do Dolar: ");
+            Cotação = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira a quantia de Dolares: ");
+            Dolar = double.Parse(Console.ReadLine());
+
+            Real = Cotação * Dolar;
+
+        
+
+            Console.WriteLine("A Quantia em Reais é {0}.", Real);
+        }
+    }
+}
